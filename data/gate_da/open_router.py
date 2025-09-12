1975aad022e35740af6926b7991a2523d7d86b8c
Amit Pratap <00amitpratap@gmail.com>
2025-09-12 20:51:43 +0530

    daily

--- data/gate_da/open_router.py
+++ data/gate_da/open_router.py
@@ -6,14 +6,19 @@ client = Groq(
     api_key= "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"   #os.environ.get("GROQ_API_KEY"),
 )
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Explain the importance of fast language models",
-        }
-    ],
-    model="llama-3.3-70b-versatile",
-)
+while True:
+    question = input("You: ")
+    if question == 'quit':
+        break
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": question,
+            }
+        ],
+        model="llama-3.3-70b-versatile",
+    )
 
-print(chat_completion.choices[0].message.content)
+    print("Bot: ",chat_completion.choices[0].message.content)
+    print("="*150,'\n\n')
